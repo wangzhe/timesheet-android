8322356b35551213df2e7afa13409086ff8678d5
Jack <zwang@thoughtworks.com>
2011-10-06 20:22:54 +0800

    Jack: ruby server init

---
+++ timesheet-android-server/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TimesheetAndroidServer::Application.config.secret_token = '2a63db70c8cbb9a80bfd73ed6ce0229bc7f89664f6ea9e8549fd5450614dcc4bd7417f2e34588895124f4de42a91db48c03c9efa9565c2f68d6479d7764d6b1c'
