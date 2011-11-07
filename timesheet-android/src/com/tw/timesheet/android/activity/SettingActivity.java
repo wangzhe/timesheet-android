cf411cdb05323751ab129f65ffefb7ac6f869267
Jack <zwang@thoughtworks.com>
2011-11-07 20:36:56 +0800

    Jack: add setting activity framework

--- timesheet-android/src/com/tw/timesheet/android/activity/SettingActivity.java
+++ timesheet-android/src/com/tw/timesheet/android/activity/SettingActivity.java
@@ -1,10 +1,16 @@
 package com.tw.timesheet.android.activity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import com.tw.timesheet.android.R;
 import com.tw.timesheet.android.activity.callback.SettingActivityView;
+import com.tw.timesheet.android.domain.StatusData;
 
 public class SettingActivity extends TimeSheetActivity implements SettingActivityView {
+
+    private Button saveButton;
+    private Button cancelButton;
+    private StatusData statusData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,10 +21,11 @@ public class SettingActivity extends TimeSheetActivity implements SettingActivit
     }
 
     private void initUI() {
-
+        saveButton = (Button) findViewById(R.id.setting_screen_save_button);
+        cancelButton = (Button) findViewById(R.id.setting_screen_cancel_button);
     }
 
     private void bindDate() {
-
+        statusData = (StatusData) getIntent().getSerializableExtra("statusData");
     }
 }
